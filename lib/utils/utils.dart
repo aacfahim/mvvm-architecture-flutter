c2bbe282ea9154238ecd348bcc183af3b20b4719
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-04-05 12:01:57 +0600

    snackbar and toast

---
+++ lib/utils/utils.dart
@@ -0,0 +1,37 @@
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:another_flushbar/flushbar_route.dart';
+
+class Utils {
+  toastMessage(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.CENTER,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.red,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
+  static void flushBarErrorMessage(String message, BuildContext context) {
+    showFlushbar(
+      context: context,
+      flushbar: Flushbar(
+        message: message,
+        backgroundColor: Colors.red,
+        duration: const Duration(seconds: 3),
+      )..show(context),
+    );
+  }
+
+  static showSnackbar(String message, BuildContext context) {
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+}
