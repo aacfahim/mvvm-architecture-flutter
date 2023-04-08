841b395c41e8130d481376c3a3b6276218b3143e
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-04-08 09:09:39 +0600

    change of name

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mvvm_arch/utils/routes/routes_name.dart';
-import 'package:mvvm_arch/view/home_screen.dart';
-import 'package:mvvm_arch/view/login_screen.dart';
+import 'package:mvvm_arch/view/home_view.dart';
+import 'package:mvvm_arch/view/login_view.dart';
 
 class Routes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RoutesName.home:
-        return MaterialPageRoute(builder: (context) => HomeScreen());
+        return MaterialPageRoute(builder: (context) => HomeView());
 
       case RoutesName.login:
-        return MaterialPageRoute(builder: (context) => LoginScreen());
+        return MaterialPageRoute(builder: (context) => LoginView());
 
       default:
         return MaterialPageRoute(builder: (context) {
