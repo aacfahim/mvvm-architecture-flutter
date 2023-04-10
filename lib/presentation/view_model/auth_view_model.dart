87b19ad26594b38c4375362908bf9d7dbad325a4
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-04-10 12:16:37 +0600

    signup api

--- lib/presentation/view_model/auth_view_model.dart
+++ lib/presentation/view_model/auth_view_model.dart
@@ -7,10 +7,17 @@ import 'package:mvvm_arch/utils/utils.dart';
 class AuthViewModel with ChangeNotifier {
   final _myRepo = AuthRepository();
   bool _loading = false;
+  bool _signUploading = false;
 
   bool get loading => _loading;
+  bool get signUpLoading => _signUploading;
 
   setLoading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
+
+  setsignUpLoading(bool value) {
     _loading = value;
     notifyListeners();
   }
@@ -22,7 +29,7 @@ class AuthViewModel with ChangeNotifier {
       //Utils.flushBarErrorMessage(value.toString(), context);
 
       if (value.toString().contains("token")) {
-        Utils.flushBarErrorMessage("Login Successfully", context);
+        Utils.flushBarErrorMessage(" Successfully", context);
         Navigator.pushNamed(context, RoutesName.home);
       } else {
         Utils.flushBarErrorMessage(value.toString(), context);
@@ -30,6 +37,27 @@ class AuthViewModel with ChangeNotifier {
       print(value.toString());
     }).onError((error, stackTrace) {
       setLoading(false);
+
+      Utils.flushBarErrorMessage(error.toString(), context);
+      print(error.toString());
+    });
+  }
+
+  Future<void> signUpApi(dynamic data, BuildContext context) async {
+    setsignUpLoading(true);
+    _myRepo.signUpApi(data).then((value) {
+      setsignUpLoading(false);
+      //Utils.flushBarErrorMessage(value.toString(), context);
+
+      if (value.toString().contains("token")) {
+        Utils.flushBarErrorMessage("Registration Successfully", context);
+        Navigator.pushNamed(context, RoutesName.home);
+      } else {
+        Utils.flushBarErrorMessage(value.toString(), context);
+      }
+      print(value.toString());
+    }).onError((error, stackTrace) {
+      setsignUpLoading(false);
 
       Utils.flushBarErrorMessage(error.toString(), context);
       print(error.toString());
