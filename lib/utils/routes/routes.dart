1b0172eaeadfdbea9eb9b88812602ab9f34d6c4e
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-04-10 12:16:58 +0600

    signup route added

--- lib/utils/routes/routes.dart
+++ lib/utils/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mvvm_arch/presentation/view/signup_view.dart';
 import 'package:mvvm_arch/utils/routes/routes_name.dart';
 import 'package:mvvm_arch/presentation/view/home_view.dart';
 import 'package:mvvm_arch/presentation/view/login_view.dart';
@@ -11,6 +12,9 @@ class Routes {
 
       case RoutesName.login:
         return MaterialPageRoute(builder: (context) => LoginView());
+
+      case RoutesName.signup:
+        return MaterialPageRoute(builder: (context) => SignUpView());
 
       default:
         return MaterialPageRoute(builder: (context) {
--- lib/utils/routes/routes_name.dart
+++ lib/utils/routes/routes_name.dart
@@ -2,6 +2,9 @@ class RoutesName {
 //accounts routes name
   static const String login = "login_view";
 
+  //accounts routes name
+  static const String signup = "signup_view";
+
 //home screen routes name
   static const String home = "home_view";
 }
