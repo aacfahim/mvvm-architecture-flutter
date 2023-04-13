513183afc02391c2b4607d33503d8b68a0f73da6
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-04-13 10:13:11 +0600

    home view

--- lib/presentation/view/home_view.dart
+++ lib/presentation/view/home_view.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mvvm_arch/presentation/view_model/user_view_model.dart';
+import 'package:mvvm_arch/utils/routes/routes_name.dart';
+import 'package:provider/provider.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final userPreference = Provider.of<UserViewModel>(context);
     return Scaffold(
       body: Column(
-        children: [],
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+              onPressed: () {
+                userPreference.remove().then((value) {
+                  Navigator.pushReplacementNamed(context, RoutesName.login);
+                });
+              },
+              child: const Text("Logout")),
+        ],
       ),
     );
   }
