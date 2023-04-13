1a8f5dc9156eb7ec64ad2690fb27aef65d1a8ba6
Fahim <ashfaq.itsd@goldenharvestbd.com>
2023-04-13 10:13:56 +0600

    shared preferences method

---
+++ lib/presentation/view_model/user_view_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:mvvm_arch/data/model/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UserViewModel with ChangeNotifier {
+  Future<bool> saveUser(UserModel user) async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+
+    sharedPreferences.setString('token', user.token.toString());
+    notifyListeners();
+
+    return true;
+  }
+
+  Future<UserModel> getUser() async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    final String? token = sharedPreferences.getString("token");
+
+    return UserModel(token: token.toString());
+  }
+
+  Future<bool> remove() async {
+    final SharedPreferences sharedPreferences =
+        await SharedPreferences.getInstance();
+    sharedPreferences.remove("token");
+    return true;
+  }
+}
